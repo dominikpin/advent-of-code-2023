c7d4c61c61644479e96fbc24df4e58fa0b0cfaae
dominikpin <dp4550@student.uni-lj.si>
2023-12-06 10:50:40 +0100

    day 6 complited

---
+++ day06/Problem_1.java
@@ -0,0 +1,52 @@
+package day06;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Problem_1 {
+
+    public Problem_1() throws FileNotFoundException {
+        File myObj = new File("day06\\input.txt");
+        Scanner myReader = new Scanner(myObj);
+        ArrayList<ArrayList<Integer>> timeAndDistance = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            timeAndDistance.add(stringToIntArray(myReader.nextLine().split(":")[1].trim().split(" ")));
+        }
+        int sum = calculateNumberOfWays(timeAndDistance);
+        myReader.close();
+        System.out.println(sum);
+    }
+
+    private static ArrayList<Integer> stringToIntArray(String[] stringArray) {
+        ArrayList<Integer> intArray = new ArrayList<>();
+        for (int i = 0; i < stringArray.length; i++) {
+            if (!stringArray[i].isEmpty()) {
+                intArray.add(Integer.parseInt(stringArray[i]));
+            }
+        }
+        return intArray;
+    }
+
+    private static int calculateNumberOfWays(ArrayList<ArrayList<Integer>> timeAndDistance) {
+        int sum = 0;
+        for (int i = 0; i < timeAndDistance.get(0).size(); i++) {
+            int time = timeAndDistance.get(0).get(i);
+            int distance = timeAndDistance.get(1).get(i);
+            for (int j = time/2; j >= 0; j--) {
+                if (j*(time-j) <= distance) {
+                    int optimalTime = time - (2*j) - 1;
+                    if (sum == 0) {
+                        sum = optimalTime;
+                    } else {
+                        sum *= optimalTime;
+                    }
+                    break;
+                }
+            }
+            
+        }
+        return sum;
+    }
+}
---
+++ day06/Problem_2.java
@@ -0,0 +1,29 @@
+package day06;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Problem_2 {
+
+    public Problem_2() throws FileNotFoundException {
+        File myObj = new File("day06\\input.txt");
+        Scanner myReader = new Scanner(myObj);
+        long[] timeAndDistance = {Long.parseLong(myReader.nextLine().split(":")[1].replaceAll("\\s+","")), Long.parseLong(myReader.nextLine().split(":")[1].replaceAll("\\s+",""))};
+        long sum = calculateNumberOfWays(timeAndDistance);
+        myReader.close();
+        System.out.println(sum);
+    }
+
+    private static long calculateNumberOfWays(long[] timeAndDistance) {
+        long time = timeAndDistance[0];
+        long distance = timeAndDistance[1];
+        for (long j = time/2; j >= 0; j--) {
+            if (j*(time-j) <= distance) {
+                long optimalTime = time - (2*j) - 1;
+                return optimalTime;
+            }
+        }
+        return 0;
+    }
+}
