0fb7f6f449f2fb07a78313591b3468a2e12db427
dominikpin <dp4550@student.uni-lj.si>
2023-12-15 20:03:55 +0100

    day 12 complited

--- day12/Part2.java
+++ day12/Part2.java
@@ -2,14 +2,19 @@ package day12;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Part2 {
 
+    private static Map<int[], Long> memoTable = new HashMap<>();
+
     public Part2() throws FileNotFoundException {
-        File myObj = new File("day12\\test.txt");
+        File myObj = new File("day12\\input.txt");
         Scanner myReader = new Scanner(myObj);
-        int sum = 0;
+        long sum = 0;
         while (myReader.hasNextLine()) {
             sum += analyzeString((myReader.nextLine()));
         }
@@ -17,31 +22,20 @@ public class Part2 {
         System.out.println(sum);
     }
 
-    private static int analyzeString(String line) {
+    private static long analyzeString(String line) {
         String[] split = line.split(" ");
         int[] numbers = stringToIntArray(split[1].split(","));
-        int sum = calcualteNumberOfWays1(split[0], numbers, 0, 0, 0);
-        System.out.println(sum);
-        // int damagedSprings = 0;
-        // for (int i = 0; i < numbers.length; i++) {
-        //     damagedSprings += numbers[i];
-        // }
-        // damagedSprings *= 5;
-        // int[] springNumbers = new int[numbers.length * 5];
-        // String springsLine = "";
-        // for (int i = 0; i < 5; i++) {
-        //     springsLine += split[0] + "?";
-        //     for (int j = 0; j < numbers.length; j++) {
-        //         springNumbers[i * numbers.length + j] = numbers[j];
-        //     }
-        // }
-        // springsLine = springsLine.substring(0, springsLine.length()-1);
-        // for (int i = 0; i < split[0].length(); i++) {
-        //     if (split[0].charAt(i) == '#') {
-        //         damagedSprings -= 5;
-        //     }
-        // }
-        // int sum = calcualteNumberOfWays(damagedSprings, springNumbers, springsLine, 0);
+        int[] springNumbers = new int[numbers.length * 5];
+        String springsLine = "";
+        for (int i = 0; i < 5; i++) {
+            springsLine += split[0] + "?";
+            for (int j = 0; j < numbers.length; j++) {
+                springNumbers[i * numbers.length + j] = numbers[j];
+            }
+        }
+        springsLine = springsLine.substring(0, springsLine.length()-1);
+        long sum = calcualteNumberOfWays(springsLine, springNumbers, 0, 0, 0);
+        memoTable.clear();
         return sum;
     }
 
@@ -53,111 +47,39 @@ public class Part2 {
         return newArray;
     }
 
-    private static int calcualteNumberOfWays1(String springsLine, int[] numbers, int i, int numbersI, int currentBroken) {
-        int sum = 0;
-        if (currentBroken > numbers[numbersI]) {
-            return 0;
+    private static long calcualteNumberOfWays(String springsLine, int[] numbers, int i, int numbersI, int currentBroken) {
+        for (Map.Entry<int[], Long> entry : memoTable.entrySet()) {
+            int[] key = entry.getKey();
+            if (Arrays.equals(key, new int[]{i, numbersI, currentBroken})) {
+                return entry.getValue();
+            }
         }
+        long sum = 0;
         if (i == springsLine.length()) {
-            if (numbersI >= numbers.length - 1) {
+            if (numbersI == numbers.length && currentBroken == 0 || numbersI == numbers.length - 1 && currentBroken != 0 && numbers[numbersI] == currentBroken) {
+                memoTable.put(new int[]{i, numbersI, currentBroken}, 1l);
                 return 1;
             }
+            memoTable.put(new int[]{i, numbersI, currentBroken}, 0l);
+            return 0;
+        }
+        if (numbersI > numbers.length) {
+            memoTable.put(new int[]{i, numbersI, currentBroken}, 0l);
             return 0;
         }
         if (springsLine.charAt(i) != '.') {
-            sum += calcualteNumberOfWays1(springsLine, numbers, i + 1, numbersI, currentBroken + 1);
+            sum += calcualteNumberOfWays(springsLine, numbers, i + 1, numbersI, currentBroken + 1);
         }
         if (springsLine.charAt(i) != '#') {
             if (currentBroken != 0) {
-                if (currentBroken == numbers[numbersI]) {
-                    sum += calcualteNumberOfWays1(springsLine, numbers, i + 1, numbersI + 1, 0);
+                if (numbersI < numbers.length && currentBroken == numbers[numbersI]) {
+                    sum += calcualteNumberOfWays(springsLine, numbers, i + 1, numbersI + 1, 0);
                 }
-                return 0;
+            } else {
+                sum += calcualteNumberOfWays(springsLine, numbers, i + 1, numbersI, 0);
             }
-            sum += calcualteNumberOfWays1(springsLine, numbers, i + 1, numbersI, 0);
         }
+        memoTable.put(new int[]{i, numbersI, currentBroken}, sum);
         return sum;
-    }
-
-    private static int calcualteNumberOfWays(int damagedSprings, int[] numbers, String springsLine, int charIndexToReplace) {
-        if (damagedSprings > 0 && !isDamagedSpringsPossible(numbers, springsLine)) {
-            return 0;
-        }
-        if (damagedSprings == 0) {
-            if (isDamagedSpringsCorrect(numbers, springsLine)) {
-                return 1;
-            }
-            return 0;
-        }
-        if (charIndexToReplace == springsLine.length()) {
-            return 0;
-        }
-        while (springsLine.charAt(charIndexToReplace) != '?') {
-            charIndexToReplace++;
-            if (charIndexToReplace == springsLine.length()) {
-                return 0;
-            }
-        }
-        StringBuilder stringBuilder = new StringBuilder(springsLine);
-        stringBuilder.setCharAt(charIndexToReplace, '#');
-        String modifiedString1 = stringBuilder.toString();
-        stringBuilder.setCharAt(charIndexToReplace, '.');
-        String modifiedString2 = stringBuilder.toString();
-
-        int count1 = calcualteNumberOfWays(damagedSprings - 1, numbers, modifiedString1, charIndexToReplace + 1);
-        int count2 = calcualteNumberOfWays(damagedSprings, numbers, modifiedString2, charIndexToReplace + 1);
-        return count1 + count2;
-    }
-
-    private static boolean isDamagedSpringsCorrect(int[] numbers, String springsLine) {
-        int counter = 0;
-        for (int i = 0; i < springsLine.length(); i++) {
-            if (springsLine.charAt(i) == '#') {
-                if (counter == numbers.length) {
-                    return false;
-                }
-                int counter1 = 0;
-                while (i != springsLine.length() && springsLine.charAt(i) == '#') {
-                    counter1++;
-                    i++;
-                }
-
-                if (counter1 != numbers[counter]) {
-                    return false;
-                }
-                counter++;
-            }
-        }
-        return true;
-    }
-    
-    private static boolean isDamagedSpringsPossible(int[] numbers, String springsLine) {
-        int counter = 0;
-        for (int i = 0; i < springsLine.length(); i++) {
-            if (springsLine.charAt(i) == '?') {
-                return true;
-            }
-            if (springsLine.charAt(i) == '#') {
-                if (counter == numbers.length) {
-                    return false;
-                }
-                int counter1 = 0;
-                while (i != springsLine.length() && springsLine.charAt(i) == '#') {
-                    counter1++;
-                    i++;
-                }
-                if (counter1 != numbers[counter] && (i == springsLine.length() || springsLine.charAt(i) == '.')) {
-                    return false;
-                }
-                if (counter1 > numbers[counter]) {
-                    return false;
-                }
-                if (i == springsLine.length() || springsLine.charAt(i) == '?') {
-                    return true;
-                }
-                counter++;
-            }
-        }
-        return true;
     }
 }
